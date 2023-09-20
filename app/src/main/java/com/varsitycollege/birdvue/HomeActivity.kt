ccba8d26e798882c74640272285f4cca58c06cff
Daniel Pienaar <dpienaar001@gmail.com>
2023-09-20 11:12:19 +0200

    Bottom nav functionality in progress

--- app/src/main/java/com/varsitycollege/birdvue/HomeActivity.kt
+++ app/src/main/java/com/varsitycollege/birdvue/HomeActivity.kt
@@ -2,10 +2,55 @@ package com.varsitycollege.birdvue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.varsitycollege.birdvue.databinding.ActivityHomeBinding
+import com.varsitycollege.birdvue.ui.CommunityFragment
+import com.varsitycollege.birdvue.ui.HotspotFragment
+import com.varsitycollege.birdvue.ui.ObservationsFragment
+import com.varsitycollege.birdvue.ui.SettingsFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Default fragment
+        replaceFragment(HotspotFragment())
+
+//        bottomNavigationView = binding.bottomNavView
+//        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+//            when(menuItem.itemId) {
+//                R.id.navigation_community -> {
+//                    replaceFragment(CommunityFragment())
+//                    true
+//                }
+//                R.id.navigation_hotspot -> {
+//                    replaceFragment(HotspotFragment())
+//                    true
+//                }
+//                R.id.navigation_observations -> {
+//                    replaceFragment(ObservationsFragment())
+//                    true
+//                }
+//                R.id.navigation_settings -> {
+//                    replaceFragment(SettingsFragment())
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.frameContainer.id, fragment).addToBackStack(null).commit()
+    }
+
 }
