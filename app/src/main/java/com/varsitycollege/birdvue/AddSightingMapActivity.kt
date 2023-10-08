f642483b6737d7d29117bed18a7209095612785f
LiamMarkEdwards <liam.mark.edwards@gmail.com>
2023-10-08 14:07:54 +0200

    added strings and check button for open gallery

--- app/src/main/java/com/varsitycollege/birdvue/AddSightingMapActivity.kt
+++ app/src/main/java/com/varsitycollege/birdvue/AddSightingMapActivity.kt
@@ -3,23 +3,25 @@ package com.varsitycollege.birdvue
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.varsitycollege.birdvue.databinding.ActivityAddSightingMapBinding
 
 class AddSightingMapActivity : AppCompatActivity() {
-    private val galleryPermission = 101
+    private val galleryPermission = Manifest.permission.READ_EXTERNAL_STORAGE
     private lateinit var binding: ActivityAddSightingMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddSightingMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.openGalleryButton.setOnClickListener {
-            if (isGalleryPermissionSuccess()) {
+            if (isGalleryPermissionGranted()) {
                 openGallery()
             } else {
                 requestGalleryPermission()
@@ -27,27 +29,38 @@ class AddSightingMapActivity : AppCompatActivity() {
         }
     }
 
-    private fun isGalleryPermissionSuccess(): Boolean {
-        return (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+    private fun isGalleryPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this, galleryPermission) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun requestGalleryPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), galleryPermission)
+        ActivityCompat.requestPermissions(this, arrayOf(galleryPermission), galleryPermissionRequestCode)
     }
+
+    private val galleryPermissionRequestCode = 101
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == galleryPermission) {
+        if (requestCode == galleryPermissionRequestCode) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openGallery()
             } else {
-                Toast.makeText(this, "Gallery permission is required to open this galery", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Gallery permission is required to open this gallery", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    private val openGalleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // Handle the result when an image is selected from the gallery
+            val data: Intent? = result.data
+            // Handle the selected image here
+        }
+    }
+
     private fun openGallery() {
         val galleryIntent = Intent(Intent.ACTION_PICK)
         galleryIntent.type = "image/*"
-        startActivityForResult(galleryIntent, galleryPermission)
+        openGalleryLauncher.launch(galleryIntent)
     }
 }
