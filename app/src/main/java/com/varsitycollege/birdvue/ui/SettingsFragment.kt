20f9bf596b84afeacc3d063614295a23662e7660
Liam2001-bit <liam.mark.edwards@gmail.com>
2023-10-18 15:49:09 +0200

    added the updateMaxDistance and UpdateMetricUnits and GetMaxDistance methods

--- app/src/main/java/com/varsitycollege/birdvue/ui/SettingsFragment.kt
+++ app/src/main/java/com/varsitycollege/birdvue/ui/SettingsFragment.kt
@@ -2,10 +2,12 @@ package com.varsitycollege.birdvue.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
@@ -15,6 +17,9 @@ import com.google.firebase.database.FirebaseDatabase
 import com.varsitycollege.birdvue.LoginActivity
 import com.varsitycollege.birdvue.databinding.FragmentSettingsBinding
 import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class SettingsFragment : Fragment() {
 
@@ -23,6 +28,7 @@ class SettingsFragment : Fragment() {
     private lateinit var database: FirebaseDatabase
     private lateinit var ref: DatabaseReference
     private lateinit var metricUnitsCheckbox: MaterialCheckBox
+    private lateinit var maxDistanceEditText: EditText
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
@@ -42,6 +48,8 @@ class SettingsFragment : Fragment() {
         // TODO Liam: change this to fetch the boolean value from the user's firebase object (see method below)
         metricUnitsCheckbox.isChecked = true
 
+
+
         //Handle sign out
         binding.logoutButton.setOnClickListener {
             auth = FirebaseAuth.getInstance()
@@ -50,6 +58,18 @@ class SettingsFragment : Fragment() {
                 logOut()
             }
         }
+    // Handle the update max distance
+        binding.updateMaxDistanceButton.setOnClickListener {
+            val maxDistanceText = binding.maxDistanceEditText.text.toString()
+            if (maxDistanceText.isNotBlank()) {
+                val maxDistanceValue = maxDistanceText.toInt()
+                // store the input into a variable and then return the method here
+                updateMaxDistance(maxDistanceValue)
+            } else {
+                Toast.makeText(requireContext(), "Please enter a valid value", Toast.LENGTH_LONG).show()
+            }
+        }
+
 
         //Handle delete account
         binding.deleteAccountButton.setOnClickListener {
@@ -147,18 +167,63 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    //TODO: Liam pls implement this :)
-    //Similarly to how updateMetricUnits works. Please add a confirm/update button to ui, and call this when that is pressed, getting the data from the maxDistanceEditText
     private fun updateMaxDistance(dist: Int) {
+        auth = FirebaseAuth.getInstance()
+        try {
+            val id = auth.currentUser?.uid
+            database = FirebaseDatabase.getInstance("https://birdvue-9288a-default-rtdb.europe-west1.firebasedatabase.app/")
+            ref = database.getReference("users")
+            if (id != null) {
 
+                ref.child(id).child("maxDistance").setValue(dist)
+            }
+        } catch (e: Exception) {
+            Toast.makeText(
+                this@SettingsFragment.requireActivity().applicationContext,
+                e.localizedMessage,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
+
 
     //TODO: And these
     //This should fetch and return the max distance from the user's object and update the value in the maxDistanceEditText when the fragment is displayed
     private fun getMaxDistance(): Int {
+        auth = FirebaseAuth.getInstance()
+        val id = auth.currentUser?.uid
 
+        if (id != null) {
+            database = FirebaseDatabase.getInstance("https://birdvue-9288a-default-rtdb.europe-west1.firebasedatabase.app/")
+            ref = database.getReference("users").child(id).child("maxDistance")
+
+            // Add a ValueEventListener to get the maxDistance value
+            // Link: https://stackoverflow.com/questions/42986449/firebase-value-event-listener
+            // date: 17 October 2023
+            ref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        val maxDistance = dataSnapshot.getValue(Int::class.java)
+                        if (maxDistance != null) {
+                            val maxDistanceText = maxDistance.toString()
+                            val editableMaxDistance = Editable.Factory.getInstance().newEditable(maxDistanceText)
+                            maxDistanceEditText.text = editableMaxDistance
+                        }
+
+                    }
+
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    //any error messages can go here
+                    val errorMessage = "database error: " + databaseError.message
+                    Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
         return 0
     }
+
 
     //this should fetch the boolean value from the user's firebase object and update the checkbox when the fragment is displayed
     private fun getMetricUnits(): Boolean {
