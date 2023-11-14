8a99e2e6731c3a32f28f3eebd31d54091e493228
Daniel Pienaar <dpienaar001@gmail.com>
2023-11-14 12:42:34 +0200

    Fixed small ui issues

--- app/src/main/java/com/varsitycollege/birdvue/ui/SettingsFragment.kt
+++ app/src/main/java/com/varsitycollege/birdvue/ui/SettingsFragment.kt
@@ -8,17 +8,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.varsitycollege.birdvue.LoginActivity
 import com.varsitycollege.birdvue.databinding.FragmentSettingsBinding
-import com.google.android.material.checkbox.MaterialCheckBox
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
